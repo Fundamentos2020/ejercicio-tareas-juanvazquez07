7e28c4dc7fd383017c30c3809ce616bac5d8f2ae
Juan <juancarlosvazquezgarcia07@gmail.com>
2020-05-10 11:55:30 -0500

    Primer commit

---
+++ indexConsulta.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylesConsulta.css">
+</head>
+<body>
+    <div class="">
+      <h1 class="text-center div-header pt-4 pb-4 m-0 text-withe">
+      Lista tareas
+      </h1>
+    </div>
+    <div class="forms-container" id="form" >
+        <h3 class="m-0 text-withe">Categoria</h3>
+        <select name="cs" id="type" class="">
+        <option value="TODOS" id="ty">Todas</option>
+        <option value="NORMAL" id="ty">Categoria 1</option>
+        <option value="FIRE" id="ty">Categoria 2</option>
+       </select>
+    </div>
+</body>
+</html>
